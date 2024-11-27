9149bfeb71ef35093f911ab02e42d62fadecfb64
Sudhish-14 <sudhish1408@gmail.com>
2024-11-27 17:00:43 +0530

    fils added

---
+++ ConstructorFunction.js
@@ -0,0 +1,7 @@
+function num(a,b){
+    this.A=a;
+    this.B=b;
+}
+let c=new num(5,10)
+console.log(c.A)
+console.log(c.B)
---
+++ Functions.js
@@ -0,0 +1,42 @@
+// //Ananymous
+
+// const fun=function(){
+//     console.log("Anonymous function")
+// }
+// // fun()
+ 
+
+// //Arrow  
+
+// const a= (()=>{
+// console.log("Arrow function")
+// })
+// // a()
+
+// //Imeadiate invoked function
+
+// (function (){
+//     console.log("Imediate invoked function")
+
+// })();
+// // function man( name,age){51
+// //     this.Name=name
+// //     this.Age=age
+// // }
+// // const ma=new man("raj",25)
+// // // console.log(ma.Name)
+// // // console.log(ma.Age)
+// // console.log(ma)
+
+
+//Array practice
+let a1="sam";
+let a2="ram";
+let a3="tom";
+
+let a=[a1,a2,a3]
+for(let b of a){
+    // if(b==a[0])
+        console.log(b)
+    
+}
